0915a863f1ff4702b27ed833a6b088bff7d9dc24
Peter Musolino <pmusolino@rms.com>
2018-04-04 14:40:34 -0400

    Adding comments

--- Apple/MacIKEv2SplitDNSTunnel.sh
+++ Apple/MacIKEv2SplitDNSTunnel.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# v1
+# This script is intended to run post an IKEv2 VPN tunnel connection
+# If you are not using Apple profiles to set split dns tunnel information this script can be run to inject
+# the supplemnentalMatchDomains setting required to make the VPN resolution function.
+# The script will grab the last DNS entry from the scutil data which *should* be the VPN DNS
+# and adds the specified key and domains to the key 
 DNS=`sudo scutil << EOF
 list ".*DNS"
 EOF`
